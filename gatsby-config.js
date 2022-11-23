3ebfe1f2f103acccde53187cc6d549b2f4863dc5
rhuan.resende <rhuan.resende@g.globo>
2022-11-23 20:30:55 -0300

    cleaunp file

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My-Blog`,
+    description: `A blog about Fullstack-developer and other cool stuff`,
+    author: `Rhuan Lennno`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,8 @@ import Seo from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404</h1>
+    <p>Ops...não tem nada aqui</p>
   </Layout>
 )
 
